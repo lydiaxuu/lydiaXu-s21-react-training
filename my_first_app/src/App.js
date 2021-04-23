f2881bb24907833f17381bd39df21301b7097c36
Lydia Xu <lydiaxu@Lydias-MacBook.local>
2021-04-22 18:02:16 -0700

    midnight-snack

---
+++ my_first_app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Snack() {
+  const midnight_snack = "smoothies & fruit";
+  return (
+    midnight_snack
+  );
+}
+
+export default Snack;
